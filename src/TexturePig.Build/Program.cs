5ebcc6fba410ce7f307d55b685f680e0a94cb6e7
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-12-05 16:57:20 +0100

    First build.

---
+++ src/TexturePig.Build/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TexturePig.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.Write(new FigletText("TexturePig").LeftAligned());
+
+        }
+    }
+}
