2c7ee89945d09ec2618ef24d5ff847dd2ab5a166
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2022-01-14 21:11:28 +0100

    Added Search and more...

---
+++ src/TexturePig.Web/Services/FragmentConvert.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TexturePig.Web.Services
+{
+    public class FragmentConvert
+    {
+        /*public static RenderFragment UrlFragment(string url) 
+        {
+            return "< img alt = \"banner\"  src = \"@urlstream\" >";
+        }*/
+    }
+}
